564d0918b2ea5da3adfb49a17a4b11994d0ade08
Guilherme Augusto Gomes Cunha <41303580+CunhaGuilhermeBR@users.noreply.github.com>
2021-11-18 19:50:34 -0300

    ValidateCep

--- methods/actions.js
+++ methods/actions.js
@@ -7,15 +7,18 @@ var functions = {
             persons = parseInt(persons) + 1
             return income / persons
         }
-
     },
     validateCep: function(cep) {
-        try {
-            cepInt = parseInt(cep)
-            return cep.length == 8
-        } catch (error) {
+        var objER = /^[0-9]{8}$/;
+
+        cep = trim(cep)
+        if (cep.length > 0) {
+            if (objER.test(cep))
+                return true;
+            else
+                return false;
+        } else
             return false
-        }
     },
     showData: async function(cep, incomeCapita, name, res) {
         const api = await returnJson(cep)
@@ -35,6 +38,10 @@ var functions = {
 async function returnJson(cep) {
     const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`)
     return response
+}
+
+function trim(str) {
+    return str.replace(/^s+|s+$/g, '');
 }
 
 module.exports = functions
